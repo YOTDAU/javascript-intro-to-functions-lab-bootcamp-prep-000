d7f6dfeac9389fa643982e7b8d6ec10c4f6ffc77
Simon Tucker <sjtucker28@gmail.com>
2019-06-19 15:22:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,14 +18,12 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
     return "I can't hear you!"
   }
-}
-
-function sayHiToGrandma(string) {
+  
   if (string.toUpperCase( === string) {
     return "YES INDEED!"
   }
   
-  if (string.to)
+
   
 
   
