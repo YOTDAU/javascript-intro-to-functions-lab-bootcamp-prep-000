140840301f1d59581509b005c1e8e8090852d076
Simon Tucker <sjtucker28@gmail.com>
2019-06-19 15:20:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,17 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase()) === string {
+  if (string.toLowerCase() === string) {
     return "I can't hear you!"
   }
+}
+
+function sayHiToGrandma(string) {
+  if (string.toUpperCase( === string) {
+    return "YES INDEED!"
+  }
+}
+
+function sayHiToGrandma(string) {
+  return 
 }
